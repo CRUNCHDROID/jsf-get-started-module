2b1cb6f75c072ebd95c325aa2c8e637a7f62e568
Riad YOUSFI <Riad YOUSFI@riad-PC>
2015-11-15 21:27:26 +0100

    CRUD Primefaces Modal

--- jsf-get-started-web/src/main/java/com/crunchdroid/controller/PrimefacesPersonController.java
+++ jsf-get-started-web/src/main/java/com/crunchdroid/controller/PrimefacesPersonController.java
@@ -3,11 +3,14 @@ package com.crunchdroid.controller;
 import com.crunchdroid.entities.Person;
 import com.crunchdroid.services.IPersonServiceLocal;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -19,7 +22,9 @@ public class PrimefacesPersonController implements Serializable {
 
     @EJB
     private IPersonServiceLocal personService;
+    
     private List<Person> people;
+    private Person selectedPerson;
 
     public PrimefacesPersonController() {
     }
@@ -38,6 +43,23 @@ public class PrimefacesPersonController implements Serializable {
 
     public void setPeople(List<Person> people) {
         this.people = people;
+    }
+
+    public Person getSelectedPerson() {
+        return selectedPerson;
+    }
+
+    public void setSelectedPerson(Person selectedPerson) {
+        this.selectedPerson = selectedPerson;
+    }
+
+    public void showPersonView() {
+        System.out.println("<<<<<<<<<<<<<<<  TEST  >>>>>>>>>>>>>>>>>>>>");
+        Map<String,Object> options = new HashMap();
+        options.put("resizable", false);
+        options.put("draggable", false);
+        options.put("modal", true);
+        RequestContext.getCurrentInstance().openDialog("Edit", options, null);
     }
 
 }
